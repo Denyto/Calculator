dd2f25df5131a54d2db9d65580fde6884a9f691e
Dzianis Tkachenka <74364128+Denyto@users.noreply.github.com>
2021-01-17 19:24:15 +0300

    init: start Calculator-task

---
+++ index.js
@@ -0,0 +1,88 @@
+let numbers = document.querySelectorAll(".number"),
+  operations = document.querySelectorAll(".operator"),
+  clearBtns = document.querySelectorAll(".btn-clear"),
+  decimalBtn = document.getElementById("decimal"),
+  result = document.getElementById("result"),
+  display = document.getElementById("display"),
+  MemoryCurrentNumber = 0,
+  MemoryNewNumber = false,
+  MemoryPendingOperation = "";
+
+for (i = 0; i < numbers.length; i++) {
+  let number = numbers[i];
+  number.addEventListener("click", function (e) {
+    numberPress(e.target.textContent);
+  });
+}
+for (i = 0; i < operations.length; i++) {
+  let number = operations[i];
+  number.addEventListener("click", function (e) {
+    operationPress(e.target.textContent);
+  });
+}
+for (i = 0; i < clearBtns.length; i++) {
+  let number = clearBtns[i];
+  number.addEventListener("click", function (e) {
+    clear(e.target.textContent);
+  });
+}
+decimalBtn.addEventListener("click", decimal);
+
+function numberPress(number) {
+  if (MemoryNewNumber) {
+    display.value = number;
+    MemoryNewNumber = false;
+  } else {
+    if (display.value === "0") {
+      display.value = number;
+    } else {
+      display.value += number;
+    }
+  }
+}
+
+function operationPress(oper) {
+  localOperationMemory = display.value;
+
+  if (MemoryNewNumber && MemoryPendingOperation !== "=") {
+    display.value = MemoryCurrentNumber;
+  } else {
+    MemoryNewNumber = true;
+    if (MemoryPendingOperation === "+") {
+      MemoryCurrentNumber += +localOperationMemory;
+    } else if (MemoryPendingOperation === "-") {
+      MemoryCurrentNumber -= +localOperationMemory;
+    } else if (MemoryPendingOperation === "*") {
+      MemoryCurrentNumber *= +localOperationMemory;
+    } else if (MemoryPendingOperation === "/") {
+      MemoryCurrentNumber /= +localOperationMemory;
+    } else {
+      MemoryCurrentNumber = +localOperationMemory;
+    }
+    display.value = MemoryCurrentNumber;
+    MemoryPendingOperation = oper;
+  }
+}
+
+ function decimal() {
+   let = localDecimalMemory = display.value;
+if (MemoryNewNumber) {
+  localDecimalMemory += '0.';
+  MemoryNewNumber = false;
+} else if (localDecimalMemory.indexOf('.') === -1) {
+localDecimalMemory += ".";
+}
+display.value = localDecimalMemory;
+ }
+
+ function clear(id) {
+   if (id === 'CE') {
+MemoryNewNumber = true;
+display.value = "0";
+   } else if (id === 'C') {
+display.value = "0";
+MemoryNewNumber = true;
+MemoryCurrentNumber = 0;
+  MemoryPendingOperation = "";
+   }
+ }
